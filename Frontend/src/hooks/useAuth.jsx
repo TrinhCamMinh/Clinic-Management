b9f590781dc2638d35d3098f5830c090cc7ab002
TrinhCamMinh <trinhcamminh25112002@gmail.com>
2024-01-06 16:55:11 +0700

    update login logic

--- Frontend/src/hooks/useAuth.jsx
+++ Frontend/src/hooks/useAuth.jsx
@@ -1,5 +1,8 @@
 const useAuth = () => {
     const user = JSON.parse(sessionStorage.getItem('userInfo'));
+
+    if (!user) return false;
+
     const isCorrectAccount = user.userName === 'minhct' && user.userPassword === 'admin';
 
     return isCorrectAccount;
